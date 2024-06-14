fa6eaa589dc82f971f27504ea50d7d98890e5739
Michele Tosi <micheletosi98@gmail.com>
2024-06-15 01:26:55 +0200

    solved code smell

--- src/main/java/it/uniroma2/tosi/utils/Costants.java
+++ src/main/java/it/uniroma2/tosi/utils/Costants.java
@@ -1,6 +1,11 @@
 package it.uniroma2.tosi.utils;
 
 public class Costants {
+
+    private Costants() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static final long COST_FP =1;
     public static final long COST_FN =10;
 
